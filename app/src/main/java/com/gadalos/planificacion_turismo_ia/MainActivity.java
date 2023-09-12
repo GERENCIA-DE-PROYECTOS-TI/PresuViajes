623d031948c98af1939a82181188eed74eb68211
Gadalos <83663953+gadalos@users.noreply.github.com>
2023-09-11 19:28:00 -0500

    Primer Commit

---
+++ app/src/main/java/com/gadalos/planificacion_turismo_ia/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gadalos.planificacion_turismo_ia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
