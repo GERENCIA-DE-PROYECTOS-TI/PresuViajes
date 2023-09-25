926b1f3ef6e241613bccffcdf161dd4f15f5a1e1
Gadalos <83663953+gadalos@users.noreply.github.com>
2023-09-25 17:04:38 -0500

    Conexion con Firebase

--- app/src/main/java/com/gadalos/planificacion_turismo_ia/LoginActivity.java
+++ app/src/main/java/com/gadalos/planificacion_turismo_ia/LoginActivity.java
@@ -3,12 +3,32 @@ package com.gadalos.planificacion_turismo_ia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText edtNombre, edtContrasena;
+    private Button btnIniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtNombre = (EditText) findViewById(R.id.edtNombre);
+        edtContrasena = (EditText) findViewById(R.id.edtContrasena);
+        btnIniciarSesion = (Button) findViewById(R.id.btnIniciarSesion);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarSesion();
+            }
+        });
+    }
+
+    public void iniciarSesion(){
+
     }
 }
