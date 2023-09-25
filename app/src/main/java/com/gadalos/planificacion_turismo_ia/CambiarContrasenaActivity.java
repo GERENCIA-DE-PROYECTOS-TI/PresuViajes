c23baa04a36637ea6eff6290759b803be42dd22f
Gadalos <83663953+gadalos@users.noreply.github.com>
2023-09-25 16:04:20 -0500

    Recuperar | Cambiar Contraseña

---
+++ app/src/main/java/com/gadalos/planificacion_turismo_ia/CambiarContrasenaActivity.java
@@ -0,0 +1,14 @@
+package com.gadalos.planificacion_turismo_ia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CambiarContrasenaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambiar_contrasena);
+    }
+}
