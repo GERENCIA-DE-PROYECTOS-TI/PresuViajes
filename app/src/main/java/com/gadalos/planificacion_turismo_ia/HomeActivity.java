b611c77e7369366edb1a05e9e47bd895f5c3f4a4
Sky Silva <70831207+Celeste18@users.noreply.github.com>
2023-10-13 02:38:57 -0500

    se subieron los recursos, se hizo vista home, vista y back tarapoto, vista y back vichayito.

---
+++ app/src/main/java/com/gadalos/planificacion_turismo_ia/HomeActivity.java
@@ -0,0 +1,9 @@
+package com.gadalos.planificacion_turismo_ia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    }
+
---
+++ app/src/main/java/com/gadalos/planificacion_turismo_ia/UsuarioActivity.java
@@ -0,0 +1,14 @@
+package com.gadalos.planificacion_turismo_ia;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario);
+    }
+}
