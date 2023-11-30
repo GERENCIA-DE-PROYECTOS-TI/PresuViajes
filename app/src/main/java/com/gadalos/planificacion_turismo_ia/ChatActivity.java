a6b58636640a4b007f2317b4685c3e0a727183b6
Gadalos <83663953+gadalos@users.noreply.github.com>
2023-11-30 01:24:20 -0500

    Color Barra de estado, WebView en para cada activity al momento de ingresar un enlace

--- app/src/main/java/com/gadalos/planificacion_turismo_ia/ChatActivity.java
+++ app/src/main/java/com/gadalos/planificacion_turismo_ia/ChatActivity.java
@@ -18,17 +18,13 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.vanniktech.emoji.EmojiPopup;
 import com.vanniktech.emoji.EmojiTextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChatActivity extends AppCompatActivity implements MessageCallback {
     private RecyclerView recyclerView;
     private EditText edtEscribirMensaje;
-    private ImageButton btnEnviar, buttonBack, emojiButton;
+    private ImageButton btnEnviar,buttonBack, emojiButton;
     private ChatAdapter chatAdapter;
     private ChatService chatService;
     private LinearLayout messageLayout;
@@ -40,33 +36,36 @@ public class ChatActivity extends AppCompatActivity implements MessageCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        // Conexion con el Firestore
+        //Conexion con el Firestore
         mAuth = FirebaseAuth.getInstance();
         mFirestore = FirebaseFirestore.getInstance();
 
         recyclerView = findViewById(R.id.recyclerView);
         edtEscribirMensaje = findViewById(R.id.escribirMensaje);
         btnEnviar = findViewById(R.id.btnEnviar);
-        emojiButton = findViewById(R.id.emojiButton);
-        messageLayout = findViewById(R.id.messageLayout);
+        emojiButton = (ImageButton) findViewById(R.id.emojiButton);
+        messageLayout = (LinearLayout) findViewById(R.id.messageLayout);
         buttonBack = findViewById(R.id.btnRegreso);
 
-        buttonBack.setOnClickListener(v -> finish());
+        buttonBack.setOnClickListener(v -> {
+            finish();
+        });
+
 
         chatAdapter = new ChatAdapter(new ArrayList<>());
         recyclerView.setAdapter(chatAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         List<Message> initialMessages = new ArrayList<>();
-        initialMessages.add(new Message("Hola, eres un asistente virtual de viajes llamado Travis", MessageRole.SYSTEM));
-        initialMessages.add(new Message("Posees solo 4 tipos de viajes en Perú: Vichayito, Tarapoto, Iquitos y Cusco", MessageRole.SYSTEM));
-        initialMessages.add(new Message("Las respuestas que brindes deben ser concretas y cortas", MessageRole.SYSTEM));
 
-        // Añadir mensajes específicos para cada destino
-        initialMessages.addAll(readMessagesForDestination("vichayito"));
-        initialMessages.addAll(readMessagesForDestination("cusco"));
-        initialMessages.addAll(readMessagesForDestination("iquitos"));
-        initialMessages.addAll(readMessagesForDestination("tarapoto"));
+        initialMessages.add(new Message("Hola, eres un asistente virtual de viajes para  llamado travis", MessageRole.SYSTEM));
+        initialMessages.add(new Message("Posees solo 4 tipos de viajes en Peru y son Vichayito, Tarapoto, Iquitos y Cusco", MessageRole.SYSTEM));
+        initialMessages.add(new Message("Vichayito es un destino encantador en la costa norte de Perú. Ofrecemos un paquete de 3 días y 2 noches que incluye alojamiento en una carpa estándar en segunda fila, desayunos y almuerzos, traslados al aeropuerto y tours de medio día y día completo. El check-in es a las 3:00 pm y el check-out es a las 12:00 pm. Si deseas más información o realizar una reserva, puedes encontrarlo en nuestra app. ¡Disfruta de tu viaje a Vichayito! ", MessageRole.SYSTEM));
+        initialMessages.add(new Message("Cusco es un destino increíble en los Andes peruanos. Ofrecemos un paquete de 5 días y 4 noches que incluye traslados a los hoteles y aeropuertos, traslado a la estación de tren, entradas a los atractivos, boleto de tren de ida y vuelta a Machu Picchu, visitas guiadas en español e inglés, almuerzos y alojamiento en hoteles seleccionados. También se incluyen desayunos diarios. ¡Esperamos que disfrutes de tu viaje a Cusco y Machu Picchu!", MessageRole.SYSTEM));
+        initialMessages.add(new Message("Iquitos es un destino fascinante en la selva amazónica del norte de Perú. Ofrecemos un paquete de 3 días y 2 noches que incluye alojamiento en el Amazonas Sinchicuy Lodge, refrescos de bienvenida y comidas durante tu estadía, traslados desde el aeropuerto/hotel, excursiones ecoturísticas y avistamiento de animales, y encuentros culturales con comunidades locales. El programa incluye visitas a la comunidad Yagua, la Laguna Huashalado, excursiones por el río Yanayacu y al proyecto de fauna silvestre en el Fundo Neiser. Si deseas obtener más información puedes buscar en nuestra app. ¡Disfruta tu aventura en la selva amazónica de Iquitos!", MessageRole.SYSTEM));
+        initialMessages.add(new Message("Tarapoto es un destino maravilloso en la selva peruana. Ofrecemos un paquete de 4 días y 3 noches que incluye alojamiento en Sumaj Casa Hotel Tarapoto, desayunos, almuerzos, traslados al aeropuerto y tours de medio día y día completo. El día 1 incluye canotaje en el río Mayo, el día 2 un tour a la Laguna Azul, el día 3 una visita al Lodge Pumarinri y las cascadas de Pucayaquillo, y el día 4 una exploración de las cataratas de Ahuashiyacu. Si deseas más información, puedes encontrarlo nuestra app. ¡Disfruta de tu viaje a Tarapoto!", MessageRole.SYSTEM));
+        initialMessages.add(new Message("Al finalizar te puedes despedir y desearle un buen viaje", MessageRole.SYSTEM));
+        initialMessages.add(new Message("Las respuestas que brindes deben ser concretas y cortas", MessageRole.SYSTEM));
 
         chatService = new ChatService(initialMessages, this);
 
@@ -79,87 +78,38 @@ public class ChatActivity extends AppCompatActivity implements MessageCallback {
         // Agregar el segundo mensaje después de 2 segundos desde el primer mensaje
         Handler handler2 = new Handler();
         handler2.postDelayed(() -> {
-            chatAdapter.addMessage(new Message("¿A dónde quieres viajar?", MessageRole.SYSTEM));
+            chatAdapter.addMessage(new Message("¿A donde quieres viajar?", MessageRole.SYSTEM));
         }, 2000);
 
         EmojiPopup emojiPopup = EmojiPopup.Builder.fromRootView(findViewById(R.id.messageLayout)).build(edtEscribirMensaje);
-        emojiButton.setOnClickListener(view -> emojiPopup.toggle());
+        emojiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emojiPopup.toggle();
+            }
+        });
+        // Dentro de onCreate o donde obtienes el usuario actual
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String userPhotoUrl = currentUser.getPhotoUrl().toString();
 
         btnEnviar.setOnClickListener(v -> {
             String messageText = edtEscribirMensaje.getText().toString().trim();
             if (!messageText.isEmpty()) {
-                // Antes de enviar el mensaje, obtener la URL de la imagen desde Firestore
-                obtenerUrlImagenFirestore(messageText);
+                Message userMessage = new Message(messageText, MessageRole.USER);
+                userMessage.setUserPhotoUrl(userPhotoUrl);
+                chatService.sendMessage(userMessage);
+                chatAdapter.addMessage(userMessage);
+                chatAdapter.addLoadingMessage();
+                EmojiTextView emojiTextView = (EmojiTextView) LayoutInflater
+                        .from(v.getContext())
+                        .inflate(R.layout.emoji_text_view, messageLayout, false);
+                messageLayout.addView(emojiTextView);
+                edtEscribirMensaje.setText("");
+                recyclerView.scrollToPosition(chatAdapter.getItemCount() - 1);
             }
         });
     }
 
-    private void obtenerUrlImagenFirestore(String messageText) {
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null) {
-            String userUid = currentUser.getUid();
-
-            mFirestore.collection("usuario").document(userUid)
-                    .get()
-                    .addOnSuccessListener(documentSnapshot -> {
-                        if (documentSnapshot.exists()) {
-                            String fotoUrl = documentSnapshot.getString("foto");
-
-                            // Crea el mensaje y asigna la URL de la imagen
-                            Message userMessage = new Message(messageText, MessageRole.USER);
-                            userMessage.setUserPhotoUrl(fotoUrl);
-
-                            // Envía el mensaje al servicio de chat
-                            chatService.sendMessage(userMessage);
-
-                            // Actualiza la interfaz de usuario
-                            chatAdapter.addMessage(userMessage);
-                            chatAdapter.addLoadingMessage();
-                            EmojiTextView emojiTextView = (EmojiTextView) LayoutInflater
-                                    .from(getApplicationContext())
-                                    .inflate(R.layout.emoji_text_view, messageLayout, false);
-                            messageLayout.addView(emojiTextView);
-                            edtEscribirMensaje.setText("");
-                            recyclerView.scrollToPosition(chatAdapter.getItemCount() - 1);
-                        }
-                    })
-                    .addOnFailureListener(e -> {
-                        // Maneja el error al obtener la URL de la imagen
-                        // Puedes mostrar un mensaje de error o tomar alguna acción apropiada
-                    });
-        }
-    }
-
-    private List<Message> readMessagesForDestination(String destination) {
-        int resourceId = getResources().getIdentifier(destination, "raw", getPackageName());
-        return readMessagesFromRaw(resourceId);
-    }
-
-    private List<Message> readMessagesFromRaw(int rawResourceId) {
-        List<Message> messages = new ArrayList<>();
-        try {
-            InputStream inputStream = getResources().openRawResource(rawResourceId);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            StringBuilder stringBuilder = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line).append("\n");
-            }
-
-            String fileContents = stringBuilder.toString();
-            String[] messagesArray = fileContents.split("---"); // Separador entre mensajes
-
-            for (String messageText : messagesArray) {
-                messages.add(new Message(messageText.trim(), MessageRole.SYSTEM));
-            }
-
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return messages;
-    }
     @Override
     public void onMessageReceived(Message message) {
         runOnUiThread(() -> {
--- app/src/main/java/com/gadalos/planificacion_turismo_ia/IquitosActivity.java
+++ app/src/main/java/com/gadalos/planificacion_turismo_ia/IquitosActivity.java
@@ -2,11 +2,20 @@ package com.gadalos.planificacion_turismo_ia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class IquitosActivity extends AppCompatActivity {
 
@@ -17,6 +26,7 @@ public class IquitosActivity extends AppCompatActivity {
 
         ImageButton infoButton = findViewById(R.id.imageButtonInfo);
         ImageButton retrocederButton = findViewById(R.id.imageButtonRetroceder);
+        String enlace = "https://tripgo.com.pe/iquitos-amazonas-sinchicuy-lodge/";
 
         retrocederButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,17 +34,75 @@ public class IquitosActivity extends AppCompatActivity {
                 // Redirigir a la actividad principal (MainActivity)
                 Intent intent = new Intent(IquitosActivity.this, HomeActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
 
         infoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Redirigir a un enlace web (https://tripgo.com.pe/)
-                Uri uri = Uri.parse("https://tripgo.com.pe/iquitos-amazonas-sinchicuy-lodge/");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+                // Mostrar el cuadro de diálogo emergente con WebView
+                showWebViewDialog(enlace);
             }
         });
+    }
+    private void showWebViewDialog(String url) {
+        // Crear el cuadro de diálogo emergente
+        final Dialog dialog = new Dialog(this, android.R.style.Theme_Black_NoTitleBar);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_webview);
+
+        // Configurar el botón de cierre
+        ImageView btnClose = dialog.findViewById(R.id.btnClose);
+        btnClose.setOnClickListener(view -> dialog.dismiss());
+
+        // Configurar la barra de progreso
+        ProgressBar progressBar = dialog.findViewById(R.id.progressBar);
+        progressBar.setMax(100); // El rango de progreso es de 0 a 100
+
+        // Configurar el botón de recarga
+        ImageView btnReload = dialog.findViewById(R.id.btnReload);
+        btnReload.setOnClickListener(view -> {
+            // Recargar la página web
+            WebView webView = dialog.findViewById(R.id.webView);
+            webView.reload();
+        });
+
+        // Configurar el WebView en el cuadro de diálogo
+        WebView webView = dialog.findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            // Este método se llama cuando la página comienza a cargarse
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                // Hacer visible la barra de progreso al comenzar la carga
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            // Este método se llama cuando la página ha terminado de cargarse
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // Hacer invisible la barra de progreso al finalizar la carga
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // Configurar la barra de progreso según el progreso de carga de la página
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+            }
+        });
+
+        // Cargar la URL en el WebView
+        webView.loadUrl(url);
+
+        // Mostrar el cuadro de diálogo
+        dialog.show();
     }
 }
